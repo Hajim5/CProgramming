df2368f36ab6090b22b0a78de83e06dea655bb4e
Yuhaa <113867217+Hajim5@users.noreply.github.com>
2025-12-10 16:00:55 +0900

    Update while.c

--- while.c
+++ while.c
@@ -1,1 +1,28 @@
+// while syntax , do~while syntax
+
+// basically is the same with "for" function which is loop but with different syntax
+//  while (" ") {  } 
+// but for while statement we need to declare the variable first before the loop 
+// and the incremenent is within the {} 
+
+#include <stdio.h>
+
+int main(void){
+  // example 
+  // delcare the variable which is number
+  int number = 1;
+  while(number <= 10){ // conditionn which is cannot more than 10
+  printf("%d", number); // will print this as the loop goes
+  number++; // increment the value of number which is within the loop
+  }
+
+// do ~ while syntax 
+// do { } while (" " );
+// example
+  do {
+    number ++;
+    printf("%d", number);
+  } while(number < 10);
+return 0;
+}
 
